126b1d0f102316c4901db2174484e9169119613a
Clement Garcin <69050287+gclement28@users.noreply.github.com>
2024-05-13 10:19:50 +0200

    adding header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,8 +3,10 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header className='header'>
-      <h1>Checkpoint : frontend</h1>
-      <Link href='/'>Countries</Link>
+      <Link href='/'>
+        <h1>Checkpoint : frontend</h1>
+      </Link>
+      <Link href='/add-country'>Add country</Link>
     </header>
   );
 }
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,10 +4,12 @@ import { ApolloProvider } from '@apollo/client';
 import client from '../graphql/client';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Header from '@/components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
+      <Header />
       <Component {...pageProps} />
     </ApolloProvider>
   );
